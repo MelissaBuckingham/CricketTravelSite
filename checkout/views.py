53955f84f1a2e7a63a593f810f4a808187ac01a4
Melissa Buckingham <m19b88@gmail.com>
2024-03-27 18:53:12 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Oz1HjP01SrkdXS4w4WbGQOlpvIXrBoZWF8EI0splDZD2EU93diBqNNDjNlYmDjLwf534sRgROvmCGOIK7V2Fqog00vvCOTPWX',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
