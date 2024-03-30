eee9157ee50ab417228bfe304e2d6927a21ea045
Melissa Buckingham <m19b88@gmail.com>
2024-03-30 12:53:42 +0000

    added profile form

--- checkout/models.py
+++ checkout/models.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
@@ -26,6 +27,7 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
 
     def _generate_order_number(self):
         """
